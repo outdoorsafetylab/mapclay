ad50adaf03ccda5990e8638d8afd9a22681dd176
Hsieh Chin Fan <pham@topo.tw>
2023-03-15 19:57:28 +0800

    Improve input UX
    - Focus text input if possible
    - Remove properties automatically if it is empty string
    - Add radio button for clear
    - Automatically refreshMap after
      * radio button is clicked
      * textArea if unfocus

--- examples/input.js
+++ examples/input.js
@@ -3,17 +3,29 @@ import 'https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.1.0/js-yaml.min.js';
 const choices = document.querySelectorAll('div[class="field"]');
 const textArea = document.querySelector('#map-text');
 
+textArea.addEventListener('focusout', (event) => {
+  refreshMap();
+});
+
 choices.forEach((choice) => {
   choice.addEventListener('click', (event) => {
     // Check radio button
-    choice.querySelector('input[type="radio"]').checked = true
+    choice.querySelector('input[type="radio"]').checked = true;
+
+    // Focus text input if possible
+    const textInput = choice.querySelector('input[type="text"]');
+    if (textInput && textInput.focus){ 
+      textInput.focus();
+    }
 
     // Get field for current option
     const field = choice.querySelector('input').name
 
     // Get value from div or text input
     var value = choice.dataset.value
-    value = value ? value : choice.querySelector('input[type="text"]').value;
+    if (! value) {
+      value = textInput ? textInput.value : ""
+    }
 
     // Change value by type
     switch (choice.dataset.type) {
@@ -38,10 +50,14 @@ choices.forEach((choice) => {
     // Get current options from textArea
     var options = jsyaml.load(textArea.value, 'utf8');
     options = options ? options : {}
-
+ 
     // Set new value
     Object.assign(options, assign)
-    textArea.value = jsyaml.dump(options);
+    removeEmptyStrings(options)
+
+    const newText = jsyaml.dump(options);
+    textArea.value = newText.startsWith('{}') ? '' : newText
+    refreshMap()
   });
 });
 
@@ -51,3 +67,16 @@ textInputs.forEach((input) => {
     input.parentElement.click()
   });
 });
+
+function removeEmptyStrings(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      removeEmptyStrings(obj[key]);
+      if (Object.keys(obj[key]).length === 0) {
+        delete obj[key]
+      }
+    } else if (obj[key] === '') {
+      delete obj[key];
+    }
+  }
+}
