b35dc990d0d073cd9f5998178502e376f379477a
Hsieh Chin Fan <pham@topo.tw>
2024-11-02 23:57:42 +0800

    style: standardjs

--- src/BasicOpenlayersRenderer.mjs
+++ src/BasicOpenlayersRenderer.mjs
@@ -64,15 +64,11 @@ const Renderer = class extends defaultExport {
     const crsString = this.validateOption('crs', crs)
       ? crs
       : (() => {
-          console.warn(
-            `Invalid Coordinate System: ${crs}, set "EPSG:4326" instead`,
-          )
+          console.warn(`Invalid Coordinate System: ${crs}, set "EPSG:4326" instead`)
           return crs
         })()
     const projection = await ol.proj.proj4.fromEPSGCode(crsString).catch(() => {
-      console.warn(
-        `Fail to retrieve Coordinate System ${crsString}, Use ${crs} instead`,
-      )
+      console.warn(`Fail to retrieve Coordinate System ${crsString}, Use ${crs} instead`)
     })
     ol.proj.setUserProjection(projection)
     return ol.proj.getUserProjection()
