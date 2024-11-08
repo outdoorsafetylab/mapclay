593f700ff9babe067464a2c81d3e0be0f351fec2
Hsieh Chin Fan <pham@topo.tw>
2024-11-08 20:57:14 +0800

    feat: update export of renderers

--- index.mjs
+++ index.mjs
@@ -1,6 +1,4 @@
-import { default as Leaflet } from './dist/renderers/leaflet.mjs'
-import { default as Maplibre } from './dist/renderers/maplibre.mjs'
-import { default as Openlayers } from './dist/renderers/openlayers.mjs'
-export const renderers = { Leaflet, Maplibre, Openlayers }
-
+export { default as Leaflet } from './dist/renderers/leaflet.mjs'
+export { default as Maplibre } from './dist/renderers/maplibre.mjs'
+export { default as Openlayers } from './dist/renderers/openlayers.mjs'
 export * from './dist/mapclay.mjs'
